ff0a4462afdcbefcd313640467c9bfcf623b4f3e
SimaMga <101292352+SimaMga@users.noreply.github.com>
2022-03-10 01:13:45 +0330

    Update utils.js

--- dapp-ui/plugins/utils.js
+++ dapp-ui/plugins/utils.js
@@ -2,7 +2,7 @@
 
 let account = null
 let libraryContract
-let libraryContractAddress = 'TD7g2t959bRjm5DFsAHYTwFrhrNLXU3gJ9' // Paste Contract address here
+let libraryContractAddress = 'TD7g2t959bRjm5DFsAHYTwFrhrNLXU3gJ9' // TXNxTBdjG5dS1vWESVKVtrSBvbNrjwNGGk
 let bookRentContract = null
 
 
